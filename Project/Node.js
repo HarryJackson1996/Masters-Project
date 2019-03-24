7ede6a870a1c9b9bd23be75a6f17cb55f77f452d
Harry W Jackson <hwjackson1996@gmail.com>
2019-03-24 20:27:48 +0000

    Added more JSDoc comments
    Docs have been created displaying documentation

--- Project/Node.js
+++ Project/Node.js
@@ -1,5 +1,18 @@
 class Node {
 
+    /**
+     * @constructor
+     * 
+     * @param {number} x - The x position of the Node object.
+     * @param {number} y - The y position of the Node object.
+     * @param {number} width - The width of the Node object.
+     * @param {number} height - The height of the Node object.
+     *
+     * @property {boolean} blocked 
+     * @property {boolean} goal 
+     * @property {boolean} start 
+     * @property {(3:number)} col
+     */
     constructor(x, y, width, height) {
         this.x = x;
         this.y = y;
@@ -11,12 +24,22 @@ class Node {
         this.start = false;
     }
 
+    /**
+     * @method
+     * @description - Method for drawing the Node graphics to the canvas.
+     */
     drawNode() {
         stroke(0);
         fill(this.col);
         rect(this.x, this.y, this.width, this.height);
     }
 
+    /**
+     * @method
+     * @description - Handles Node onClick functionality.
+     * @see setNormal 
+     * @see setBlocked
+     */
     clicked() {
         if(mouseX > this.x && mouseX < this.x + this.width){      
             if(mouseY > this.y && mouseY < this.y + this.height){ 
@@ -33,6 +56,10 @@ class Node {
         }
     }
 
+    /**
+     * @method
+     * @description - returns the node state to normal (it is traversable).
+     */
     setNormal() {
         this.col = color(255, 255, 255);
         this.goal = false;
@@ -41,41 +68,83 @@ class Node {
         return;
     }
     
+    /**
+     * @method 
+     * @description - Sets the nodes state to goal.
+     * @returns {boolean}
+     */
     setGoal() {
         this.col = color(0, 200, 0);
         this.blocked = true;
         return this.goal = true;
     }
 
+    /**
+     * @method 
+     * @description - Sets the nodes state to blocked.
+     * @returns {boolean}
+     */
     setBlocked() {
         this.col = color(200, 0, 0);
         return this.blocked = true;  
     }
 
+    /**
+     * @method 
+     * @description - Sets the nodes state to start.
+     * @returns {boolean}
+     */
     setStart() {
         this.col = color(0, 220, 255);
         return this.start = true;  
     }
 
+    /**
+     * @method 
+     * @returns {boolean} - Returns the nodes x position.
+     */
     getX(){
         return this.x;
     }
 
+    /**
+     * @method 
+     * @returns {boolean} - Returns the nodes y position.
+     */
     getY() {
         return this.y;
     }
     
+    /**
+     * @method 
+     * @returns {boolean} - Returns the nodes width.
+     */
     getWidth() {
         return this.width;
     }
-    
+ 
+    /**
+     * @method 
+     * @returns {boolean} - Returns the nodes height.
+     */
     getHeight() {
         return this.height;
     }
 }
 
+/**
+ * @memberof Node
+ */
 class BlockedNode extends Node {
 
+    /**
+     * @param {number} x - The x position of the Node object.
+     * @param {number} y - The y position of the Node object.
+     * @param {number} width - The width of the Node object.
+     * @param {number} height - The height of the Node object.
+     * 
+     * @property {number} col
+     */
     constructor(x, y, width, height){
         super(x, y, width, height);
         this.blocked = true;
@@ -83,8 +152,17 @@ class BlockedNode extends Node {
     }
 }
 
+/**
+ * @memberof Node
+ */
 class GoalNode extends Node {
 
+    /**
+     * @param {number} x - The x position of the Node object.
+     * @param {number} y - The y position of the Node object.
+     * @param {number} width - The width of the Node object.
+     * @param {number} height - The height of the Node object.
+     */
     constructor(x, y, width, height){
         super(x, y, width, height);
         this.goal = true;
@@ -93,8 +171,17 @@ class GoalNode extends Node {
     }
 }
 
+/**
+ * @memberof Node
+ */
 class StartNode extends Node {
-
+    
+    /**
+     * @param {number} x - The x position of the Node object.
+     * @param {number} y - The y position of the Node object.
+     * @param {number} width - The width of the Node object.
+     * @param {number} height - The height of the Node object.
+     */
     constructor(x, y, width, height){
         super(x, y, width, height);
         this.start = true;
--- Project/Settings.js
+++ Project/Settings.js
@@ -1,20 +1,39 @@
 class Settings {
 
+    /**
+     * @constructor
+     * 
+     * @param {Number} width - The canvas width in pixels.
+     * @param {Number} height - The canvas height in pixels.
+     * @param {Number} size - The size of a singular node (width and height).
+     */
     constructor(width, height, size) {
         this.SCREEN_HEIGHT = height;
         this.SCREEN_WIDTH = width;
         this.NODE_SIZE = size;
     }
 
+    /**
+     * @method
+     * @returns {Number} The Canvas width.
+     */
     getWidth() {
         return this.SCREEN_WIDTH + 1;
     }
 
+    /**
+     * @method
+     * @returns {Number} The Canvas height.
+     */
     getHeight() {
         return this.SCREEN_HEIGHT + 1;
     }
 
-
+    /**
+     * @method
+     * @returns {Number} The Node width and height.
+     * @see Node
+     */
     getNodeSize() {
         return this.NODE_SIZE;
     }
